9c45712b165b00e029a7c38d0e9f541e7e68bd53
jandelmi <33371584+jandelmi@users.noreply.github.com>
2018-02-24 16:48:30 +0200

    Update verreq.jl

--- deps/verreq.jl
+++ deps/verreq.jl
@@ -1,9 +1,9 @@
 # Version requirements for libglpk
 
-const glpkminver = v"4.52" # Minimum requirement
-const glpkmaxver = v"4.62" # Maximum version known to work
-const glpkdefver = "4.61"  # Default version
-const glpkwinver = "4.52"  # Default version for windows
+const glpkminver = v"4.57" # Minimum requirement
+const glpkmaxver = v"4.65" # Maximum version known to work
+const glpkdefver = "4.65"  # Default version
+const glpkwinver = "4.65"  # Default version for windows
 
 function check_glpk_version(major_ver, minor_ver)
     if !(glpkminver <= VersionNumber(major_ver, minor_ver) <= glpkmaxver)
